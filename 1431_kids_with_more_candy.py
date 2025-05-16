c727d07f41ef6d4e87a22cd72a94f837dd5f2459
DakodaStemen <dakoda.stemen@proton.me>
2025-05-15 20:28:06 -0700

    Comments for tomorrow.

---
+++ 1431_kids_with_more_candy.py
@@ -0,0 +1,9 @@
+# make sure to add all notes and comments
+
+
+# Problem: 1431. Kids With the Greatest Number of Candies
+# Link: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+# Difficulty: Easy
+# Tags: Array, Simulation
+
+# Approach:         
